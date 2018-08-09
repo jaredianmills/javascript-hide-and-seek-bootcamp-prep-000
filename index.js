bf0fa600d0e20796f8c68531272591ec0c75bbc9
jaredianmills <jaredianmills@gmail.com>
2018-08-09 08:52:40 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll(".ranked-list li")
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  let grandNode = document.querySelector("#grand-node")
+  let children = grandNode.children[0]
+  while (children.children[0]) {
+    children = children.children[0]
+  }
+  return children.innerText
+}
